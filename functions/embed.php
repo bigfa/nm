cd56e64c97267117dc93d0660b738d656ff21c0c
bigfa <jigoulee@gmail.com>
2016-04-29 11:46:57 +0800

    feat: add shortcake support

--- functions/embed.php
+++ functions/embed.php
@@ -14,7 +14,6 @@ function nm_format_time( $seconds ) {
     return $min . ':' . $second;
 }
 
-
 function nms_like_btn($id) {
     global $post;
     if(!is_singular() ) return;
@@ -34,8 +33,101 @@ wp_embed_register_handler( 'neteasemusicalbum', '#http:\/\/music\.163\.com\/\#\/
 
 wp_embed_register_handler( 'xiami','#http:\/\/www\.xiami\.com\/(\w+)\/(\d+)#i','wp_embed_handler_xiami');
 
-function nm_generate_player( $type = null, $id = null){
+function nm_generate_player( $source = null,$type = null, $id = null){
+    $source = $source ? $source : 'netease';
+    global $nmjson;
+    global $instance;
+    if($source == 'netease' ) {
+        switch ($type) {
+            case 'album':
+                $data = $nmjson->netease_album($id);
+                $songs = $data['songs'];
+                $html .= nm_single_playform( $data['album_id'] , $instance , $data['album_cover'] , $data['album_title'] , $data['album_author'] , '' , true );
+                $class = nm_get_setting('listopen') ? ' hide' : '';
 
+                $html .= '<div class="nms-list' . $class . '" id="nm-list-' . $instance . '" data-index="' . $instance . '">';
+                foreach ($songs as $key => $song) {
+                    $html .= '<div class="nms-list-item">' . $song['title'] . ' - ' . $song['artist'] . '<span class="song-time">' . nm_format_time( $song['duration'] ) . '</span></div>';
+                }
+                $html .= '</div>';
+                $html .= '<script>playlist.push(' . json_encode($songs). ');</script>';
+                break;
+
+            case 'song':
+                $data = nm_get_setting('oversea') ? $nmjson->netease_oversea_song($id) : $nmjson->netease_song($id);
+                $html .= nm_single_playform( $data['id'] , $instance , $data['cover'] , $data['title'] , $data['artist'] , $data['duration'] );
+
+
+                $html .= '<script>playlist.push(' . json_encode(array($data)). ');</script>';
+                break;
+
+            case 'playlist':
+                $data = $nmjson->netease_playlist($id);
+                $songs = $data['songs'];
+
+                $html .= nm_single_playform( $data['collect_id'] , $instance , $data['collect_cover'] , $data['collect_title'] , $data['collect_author'] , ''  , true);
+                $class = nm_get_setting('listopen') ? ' hide' : '';
+
+                $html .= '<div class="nms-list' . $class . '" id="nm-list-' . $instance . '" data-index="' . $instance . '">';
+                foreach ($songs as $key => $song) {
+                    $html .= '<div class="nms-list-item"><span class="song-info">' . $song['title'] . ' - ' . $song['artist'] . '</span><span class="song-time">' . nm_format_time( $song['duration'] ) . '</span></div>';
+                }
+                $html .= '</div>';
+                $html .= '<script>playlist.push(' . json_encode($songs). ');</script>';
+                break;
+            case 'program':
+                $data = $nmjson->netease_radio($id);
+                $html .= nm_single_playform( $data['id'] , $instance , $data['cover'] , $data['title'] , $data['artist'] , $data['duration'] );
+                $html .= '<script>playlist.push(' . json_encode(array($data)) . ');</script>';
+                break;
+            default:
+                return $url;
+                break;
+        }
+    } elseif ( $source == 'xiami') {
+        switch ($type) {
+            case 'album':
+                $data = $nmjson->xiami_album($id);
+                $songs = $data['songs'];
+                $html .= nm_single_playform( $data['album_id'] , $instance , $data['album_cover'] , $data['album_title'] , $data['album_author'] , '' , true );
+                $class = nm_get_setting('listopen') ? ' hide' : '';
+
+                $html .= '<div class="nms-list' . $class . '" id="nm-list-' . $instance . '" data-index="' . $instance . '">';
+
+                foreach ($songs as $key => $song) {
+                    $html .= '<div class="nms-list-item">' . $song['title'] . ' - ' . $song['artist'] . '<span class="song-time">' . nm_format_time( $song['duration'] ) . '</span></div>';
+                }
+                $html .= '</div>';
+                $html .= '<script>playlist.push(' . json_encode($songs). ');</script>';
+                break;
+
+            case 'song':
+                $data = $nmjson->xiami_song($id);
+                $html .= nm_single_playform( $data['id'] , $instance , $data['cover'] , $data['title'] , $data['artist'] , $data['duration'] );
+                $html .= '<script>playlist.push(' . json_encode(array($data)). ');</script>';
+                break;
+
+            case 'collect':
+                $data = $nmjson->xiami_collect($id);
+                $songs = $data['songs'];
+
+                $html .= nm_single_playform( $data['collect_id'] , $instance , $data['collect_cover'] , $data['collect_title'] , $data['collect_author'] , ''  , true);
+                $class = nm_get_setting('listopen') ? ' hide' : '';
+
+                $html .= '<div class="nms-list' . $class . '" id="nm-list-' . $instance . '" data-index="' . $instance . '">';
+
+                foreach ($songs as $key => $song) {
+                    $html .= '<div class="nms-list-item"><span class="song-info">' . $song['title'] . ' - ' . $song['artist'] . '</span></div>';
+                }
+                $html .= '</div>';
+                $html .= '<script>playlist.push(' . json_encode($songs). ');</script>';
+                break;
+            default:
+                return $url;
+                break;
+        }
+    }
+    return $html;
 }
 
 function wp_embed_handler_xiami( $matches, $attr, $url, $rawattr ){
@@ -51,47 +143,7 @@ function wp_embed_handler_xiami( $matches, $attr, $url, $rawattr ){
     $instance = $instance ? $instance : 0;
 
     if( $instance === 0) $html ='<script>var playlist = []</script><div id="nm_jplayer" style="display:none"></div>';
-    switch ($type) {
-        case 'album':
-            $data = $nmjson->xiami_album($id);
-            $songs = $data['songs'];
-            $html .= nm_single_playform( $data['album_id'] , $instance , $data['album_cover'] , $data['album_title'] , $data['album_author'] , '' , true );
-            $class = nm_get_setting('listopen') ? ' hide' : '';
-
-            $html .= '<div class="nms-list' . $class . '" id="nm-list-' . $instance . '" data-index="' . $instance . '">';
-
-            foreach ($songs as $key => $song) {
-                $html .= '<div class="nms-list-item">' . $song['title'] . ' - ' . $song['artist'] . '<span class="song-time">' . nm_format_time( $song['duration'] ) . '</span></div>';
-            }
-            $html .= '</div>';
-            $html .= '<script>playlist.push(' . json_encode($songs). ');</script>';
-            break;
-
-        case 'song':
-            $data = $nmjson->xiami_song($id);
-            $html .= nm_single_playform( $data['id'] , $instance , $data['cover'] , $data['title'] , $data['artist'] , $data['duration'] );
-            $html .= '<script>playlist.push(' . json_encode(array($data)). ');</script>';
-            break;
-
-        case 'collect':
-            $data = $nmjson->xiami_collect($id);
-            $songs = $data['songs'];
-
-            $html .= nm_single_playform( $data['collect_id'] , $instance , $data['collect_cover'] , $data['collect_title'] , $data['collect_author'] , ''  , true);
-            $class = nm_get_setting('listopen') ? ' hide' : '';
-
-            $html .= '<div class="nms-list' . $class . '" id="nm-list-' . $instance . '" data-index="' . $instance . '">';
-
-            foreach ($songs as $key => $song) {
-                $html .= '<div class="nms-list-item"><span class="song-info">' . $song['title'] . ' - ' . $song['artist'] . '</span></div>';
-            }
-            $html .= '</div>';
-            $html .= '<script>playlist.push(' . json_encode($songs). ');</script>';
-            break;
-        default:
-            return $url;
-            break;
-    }
+    $html .= nm_generate_player('xiami',$type,$id);
     $instance++;
     return apply_filters( 'embed_forbes', $html, $matches, $attr, $url, $rawattr );
 }
@@ -107,59 +159,7 @@ function wp_embed_handler_neteasemusicalbum( $matches, $attr, $url, $rawattr ) {
     global $instance;
     $instance = $instance ? $instance : 0;
     if( $instance === 0) $html ='<script>var playlist = []</script><div id="nm_jplayer" style="display:none"></div>';
-    switch ($type) {
-        case 'album':
-            $data = $nmjson->netease_album($id);
-            $songs = $data['songs'];
-            $html .= nm_single_playform( $data['album_id'] , $instance , $data['album_cover'] , $data['album_title'] , $data['album_author'] , '' , true );
-            $class = nm_get_setting('listopen') ? ' hide' : '';
-
-            $html .= '<div class="nms-list' . $class . '" id="nm-list-' . $instance . '" data-index="' . $instance . '">';
-            foreach ($songs as $key => $song) {
-                $html .= '<div class="nms-list-item">' . $song['title'] . ' - ' . $song['artist'] . '<span class="song-time">' . nm_format_time( $song['duration'] ) . '</span></div>';
-            }
-            $html .= '</div>';
-            $html .= '<script>playlist.push(' . json_encode($songs). ');</script>';
-            break;
-
-        case 'song':
-            $data = nm_get_setting('oversea') ? $nmjson->netease_oversea_song($id) : $nmjson->netease_song($id);
-            $html .= nm_single_playform( $data['id'] , $instance , $data['cover'] , $data['title'] , $data['artist'] , $data['duration'] );
-            if( nm_get_setting("comment") ) $comments = $nmjson->comments($id);
-            if (!empty($comments)) :
-                $html .= '<div class="nmhotcom"><span class="com-close">X</span><div class="nmhc-title">网易热评</div>';
-                foreach ($comments as $key => $comment) {
-                    $html .= '<div class="nmh-item"><span style="background-image:url(' . $comment['user']['avatarUrl'] . '?param=48x48)" class="nmu-avatar"></span><span class="nmu-name">' . $comment['user']['nickname'] . '</span>:' .$comment['content'] . '</div>';
-                }
-                $html .= '</div>';
-            endif;
-
-            $html .= '<script>playlist.push(' . json_encode(array($data)). ');</script>';
-            break;
-
-        case 'playlist':
-            $data = $nmjson->netease_playlist($id);
-            $songs = $data['songs'];
-
-            $html .= nm_single_playform( $data['collect_id'] , $instance , $data['collect_cover'] , $data['collect_title'] , $data['collect_author'] , ''  , true);
-            $class = nm_get_setting('listopen') ? ' hide' : '';
-            
-            $html .= '<div class="nms-list' . $class . '" id="nm-list-' . $instance . '" data-index="' . $instance . '">';
-            foreach ($songs as $key => $song) {
-                $html .= '<div class="nms-list-item"><span class="song-info">' . $song['title'] . ' - ' . $song['artist'] . '</span><span class="song-time">' . nm_format_time( $song['duration'] ) . '</span></div>';
-            }
-            $html .= '</div>';
-            $html .= '<script>playlist.push(' . json_encode($songs). ');</script>';
-            break;
-        case 'program':
-            $data = $nmjson->netease_radio($id);
-            $html .= nm_single_playform( $data['id'] , $instance , $data['cover'] , $data['title'] , $data['artist'] , $data['duration'] );
-            $html .= '<script>playlist.push(' . json_encode(array($data)) . ');</script>';
-            break;
-        default:
-            return $url;
-            break;
-    }
+    $html .= nm_generate_player('netease',$type,$id);
     $instance++;
     return apply_filters( 'embed_forbes', $html, $matches, $attr, $url, $rawattr );
 }
